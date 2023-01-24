37afea370d185993075c42b82df982fbc5f0999f
unknown <rosalinda@fullstack.ph>
2023-01-24 17:30:35 +0800

    Homepage

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'regal-blue': '#0F0F0F',
+        'font-gray': '#958C8D',
+        'font-green': '#01FF01'
+      },
+      backgroundImage: {
+        'hero_pattern': "url('/razer-nari-hero-desktop-wireless-gaming-headset CL.jpg')",
+      },
+      minHeight: {
+        'full-container':' 100vh'
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
